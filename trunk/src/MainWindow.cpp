536d2fd9a99231f2feacf7b5a3bcd5249d0a5cb6
Dario Freddi <drf54321@gmail.com>
2008-02-07 17:27:38 +0000

    Switch it!

--- trunk/src/MainWindow.cpp
+++ trunk/src/MainWindow.cpp
@@ -133,9 +133,9 @@ bool MainWindow::populatePackagesView()
 			
 			/* TODO: show icons instead of text here *///Comment: IMO we should show text and icon here (more usable) (boom1992)
 			if(aHandle->isInstalled(pkg))
-				item->setText(1, "Installed");
+				item->setText(0, "Installed");
 			else
-				item->setText(1, "Not Installed");
+				item->setText(0, "Not Installed");
 									
 			item->setText(2, alpm_pkg_get_name(pkg));
 			item->setText(3, alpm_pkg_get_version(pkg));
@@ -406,11 +406,14 @@ void MainWindow::finishDbUpdate()
 {
 	disconnect(dbdialog, 0,0,0);
 	
+	if(dbdialog->dbHasBeenUpdated())
+	{
+		pkgsViewWG->setSortingEnabled(false);
+		populatePackagesView();
+	}
+	
 	dbdialog->deleteLater();
 	
-	pkgsViewWG->setSortingEnabled(false);
-	
-	populatePackagesView();
 }
 
 void MainWindow::showContextMenu()
