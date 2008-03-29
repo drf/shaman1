5612105dcaebb73cff0c6d2399f3ea5b5c348d03
Lukas Appelhans <boom1992@gmx.de>
2008-03-29 02:19:09 +0000

    Ok let's co if that works ;)

--- trunk/shaman/plasma/applet/shamanApplet.cpp
+++ trunk/shaman/plasma/applet/shamanApplet.cpp
@@ -49,11 +49,11 @@ void ShamanApplet::init()
         kDebug()<<"Shaman Engine could not be loaded";
     }
 
-    m_layout = new Plasma::VBoxLayout(this);
-    m_actionLayout = new Plasma::HBoxLayout(m_layout);
-    m_layout->addItem(m_actionLayout);
-    m_lineLayout = new Plasma::HBoxLayout(m_layout);
-    m_layout->addItem(m_lineLayout);
+    setLayout(new Plasma::VBoxLayout(this));
+    m_actionLayout = new Plasma::HBoxLayout(this);
+    //m_layout->addItem(m_actionLayout);
+    m_lineLayout = new Plasma::HBoxLayout(this);
+    //m_layout->addItem(m_lineLayout);
     //Why are those things not shown?
     m_updateDatabaseIcon = new Plasma::Icon(KIcon("view-refresh"), tr("Update Database"), this);
     m_actionLayout->addItem(m_updateDatabaseIcon);
