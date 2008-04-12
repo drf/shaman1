f5fd3015b368d576f18cc90e2d09fe3d85e24b38
Lukas Appelhans <boom1992@gmx.de>
2008-04-12 21:17:08 +0000

    Move in some bad code, which will get removed in a few days

--- branches/Shaman2/plugins/packagesview/packagesview.h
+++ branches/Shaman2/plugins/packagesview/packagesview.h
@@ -1,1 +1,16 @@
- 
+#ifndef PACKAGESVIEW_H
+#define PACKAGESVIEW_H
+
+#include <QTreeView>
+
+class PackagesView : public QTreeView
+{
+    Q_OBJECT
+    public:
+        PackagesView();
+        ~PackagesView();
+
+        
+};
+
+#endif
--- branches/Shaman2/plugins/packagesview/packagesviewplugin.h
+++ branches/Shaman2/plugins/packagesview/packagesviewplugin.h
@@ -1,1 +1,15 @@
- 
+#ifndef PACKAGESVIEWPLUGIN_H
+#define PACKAGESVIEWPLUGIN_H
+
+#include "ShamanGuiPlugin.h"
+
+class PackagesViewPlugin : public ShamanGuiPlugin
+{
+    public:
+        PackagesViewPlugin();
+        ~PackagesViewPlugin();
+
+        void init();
+};
+
+#endif
