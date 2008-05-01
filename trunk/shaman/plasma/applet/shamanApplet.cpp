7a57190a8de7bb1f81260dd2eac51166496b4153
Lukas Appelhans <boom1992@gmx.de>
2008-05-01 21:20:47 +0000

    More plasmoid

--- trunk/shaman/plasma/applet/shamanApplet.cpp
+++ trunk/shaman/plasma/applet/shamanApplet.cpp
@@ -52,21 +52,37 @@ void ShamanApplet::init()
     }
 
     QGraphicsLinearLayout *m_layout = new QGraphicsLinearLayout(Qt::Vertical, this);
+
     QGraphicsLinearLayout *m_actionsLayout = new QGraphicsLinearLayout(m_layout);
+
     Plasma::Icon *m_updateDatabaseIcon = new Plasma::Icon(KIcon("view-refresh"), i18n("Update Database"), this);
-    connect(m_updateDatabaseIcon, SIGNAL(clicked()), SLOT(updateDatabase()));
+    connect(m_updateDatabaseIcon, SIGNAL(activated()), SLOT(updateDatabase()));
     m_actionsLayout->addItem(m_updateDatabaseIcon);
+
     Plasma::Icon *m_upgradeSystemIcon = new Plasma::Icon(KIcon("system-software-update"), i18n("Upgrade System"), this);
-    connect(m_upgradeSystemIcon, SIGNAL(clicked()), SLOT(upgradeSystem()));
+    connect(m_upgradeSystemIcon, SIGNAL(activated()), SLOT(upgradeSystem()));
     m_actionsLayout->addItem(m_upgradeSystemIcon);
+
     m_layout->addItem(m_actionsLayout);
+
     QGraphicsLinearLayout *m_lineLayout = new QGraphicsLinearLayout(m_layout);
+
     QGraphicsProxyWidget *m_lineEdit = new QGraphicsProxyWidget(this);
     m_lineEdit->setWidget(new QLineEdit(0));
     m_lineLayout->addItem(m_lineEdit);
+
     Plasma::Icon *m_actionIcon = new Plasma::Icon(KIcon("tools-wizard"), i18n("Action"), this);
+    QAction *m_installAction = new QAction(KIcon("list-add"), i18n("Install Package"), this);
+    connect(m_installAction, SIGNAL(triggered()), SLOT(installPackage()));
+    m_actionIcon->addAction(m_installAction);
+    QAction *m_removeAction = new QAction(KIcon("list-remove"), i18n("Uninstall Package"), this);
+    connect(m_removeAction, SIGNAL(triggered()), SLOT(removePackage()));
+    m_actionIcon->addAction(m_removeAction);
+
     m_lineLayout->addItem(m_actionIcon);
+
     m_layout->addItem(m_lineLayout);
+
     setLayout(m_layout);
 }
 
