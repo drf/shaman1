f60b1590fb367ee56685e5553f09673b805481fa
Dario Freddi <drf54321@gmail.com>
2008-05-02 16:50:51 +0000

    Adding mk locale

--- trunk/shaman/generateTs.sh
+++ trunk/shaman/generateTs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for arg in "fr" "it" "de" "el" "pt_BR" "es" "pl" "tr" "zh_CN"
+for arg in "fr" "it" "de" "el" "pt_BR" "es" "pl" "tr" "zh_CN" "mk"
 do
 	lupdate *.cxx src/* ui/* -ts translations/shaman_$arg.ts
 done
