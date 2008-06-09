018f7c26356d9ec1f7553f42258ae94d8c888e97
Dario Freddi <drf54321@gmail.com>
2008-06-09 18:22:33 +0000

    Adding Norwegian Translation

--- trunk/shaman/generateTs.sh
+++ trunk/shaman/generateTs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for arg in "fr" "it" "de" "el" "pt_BR" "es" "pl" "tr" "zh_CN" "mk" "cs"
+for arg in "fr" "it" "de" "el" "pt_BR" "es" "pl" "tr" "zh_CN" "mk" "cs" "nb"
 do
 	lupdate *.cxx src/* ui/* -ts translations/shaman_$arg.ts
 done
