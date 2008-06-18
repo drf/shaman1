276b67b5e10287f7cabb2347c84e420fa4fb6ae1
Dario Freddi <drf54321@gmail.com>
2008-06-18 10:04:34 +0000

    Adding yet another translation

--- trunk/shaman/generateTs.sh
+++ trunk/shaman/generateTs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for arg in "fr" "it" "de" "el" "pt_BR" "es" "pl" "tr" "zh_CN" "mk" "cs" "nb"
+for arg in "fr" "it" "de" "el" "pt_BR" "es" "pl" "tr" "zh_CN" "mk" "cs" "nb" "nl"
 do
 	lupdate *.cxx src/* ui/* -ts translations/shaman_$arg.ts
 done
