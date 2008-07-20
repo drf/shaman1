e51f7f61daa169a7219495d726203fa90ebc8d4a
Dario Freddi <drf54321@gmail.com>
2008-07-20 15:09:05 +0000

    Adding Lithuanian

--- trunk/shaman/generateTs.sh
+++ trunk/shaman/generateTs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for arg in "fr" "it" "de" "el" "pt_BR" "es" "pl" "tr" "zh_CN" "mk" "cs" "nb" "nl"
+for arg in "fr" "it" "de" "el" "pt_BR" "es" "pl" "tr" "zh_CN" "mk" "cs" "nb" "nl" "lt"
 do
 	lupdate *.cxx src/* ui/* -ts translations/shaman_$arg.ts
 done
